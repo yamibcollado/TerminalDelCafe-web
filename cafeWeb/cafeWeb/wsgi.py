e776ff40b68bcbe94e8794b79ef113735d2c4266
Yami <yamicollado@hotmail.com>
2025-08-25 12:18:35 -0300

    commit inicial

---
+++ cafeWeb/cafeWeb/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cafeWeb project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cafeWeb.settings')
+
+application = get_wsgi_application()

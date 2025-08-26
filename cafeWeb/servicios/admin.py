626495526a38e1006904e2a9815a9fcb05d5ae3f
Yami <yamicollado@hotmail.com>
2025-08-26 15:53:37 -0300

    app servicio y BD

---
+++ cafeWeb/servicios/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Servicio
+
+#se registran los modelos aca y aparece en el administrador.
+
+
+admin.site.register(Servicio)
---
+++ cafeWeb/servicios/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Servicio(models.Model):
+    titulo=models.CharField(max_length=20)
+    contenido=models.CharField(max_length=50)
+    imagen=models.ImageField()
+    precio=models.IntegerField()
+
+    class Meta:
+        verbose_name='servicio'
+        verbose_name_plural='servicios'
+    
+    def __str__(self):
+        return self.titulo
